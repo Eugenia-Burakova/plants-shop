0a8dbe99f4051322329f27971714fbb41d1e9d26
Eugenia-Burakova <evzickabur@gmail.com>
2022-08-31 19:14:09 +0000

    products-slides section started

--- javascript/products-slides.js
+++ javascript/products-slides.js
@@ -1,27 +1,39 @@
 (function() {
 
     const slides = [
-        `<div class="about-elsapo__carousel-slide">
-        <img class="about-elsapo__carousel-slide-img" src="img/about-elsapo/about-elsapo-carousel-slide-first.jpg" alt="fresh fish and vegetables">
+        `<div class="products-slides__carousel-slide">
+        <img class="products-slides__carousel-slide-img" src="img/products-slides/consultations.png" alt="plant in a pot icon">
+        <h3 class="products-slides__title">Consultations</h3>
+            <p class="products-slides__text">We not only sell plants and everything related to them 
+                but are also ready to answer any of your questions! Contact us to select the best plant!</p>
+            <a href="#" class="products-slides__learn-more">Learn More</a>
         </div>`,
 
-        `<div class="about-elsapo__carousel-slide">
-        <img class="about-elsapo__carousel-slide-img" src="img/about-elsapo/about-elsapo-carousel-slide-second.jpg" alt="rainbow vegetables">
+        `<div class="products-slides__carousel-slide">
+        <img class="products-slides__carousel-slide-img" src="img/products-slides/plant-food.png" alt="plant in a pot icon">
         </div>`,
 
-        `<div class="about-elsapo__carousel-slide">
-        <img class="about-elsapo__carousel-slide-img" src="img/about-elsapo/about-elsapo-carousel-slide-third.jpg" alt="colorful fruits">
+        `<div class="products-slides__carousel-slide">
+        <img class="products-slides__carousel-slide-img" src="img/products-slides/plants-for-garden.png" alt="cactus in a pot icon">
         </div>`,
 
-        ` <div class="about-elsapo__carousel-slide">
-        <img class="about-elsapo__carousel-slide-img" src="img/about-elsapo/about-elsapo-carousel-slide-fourth.png" alt="groats">
+        `<div class="products-slides__carousel-slide">
+        <img class="products-slides__carousel-slide-img" src="img/products-slides/plants-for-home.png" alt="flower in a pot icon">
+        </div>`
+
+        `<div class="products-slides__carousel-slide">
+        <img class="products-slides__carousel-slide-img" src="img/products-slides/sale-of-flowerpots.png" alt="plant in a pot icon">
+        </div>`
+
+        `<div class="products-slides__carousel-slide">
+        <img class="products-slides__carousel-slide-img" src="img/products-slides/soil-for-plants.png" alt="flower in a pot icon">
         </div>`
     ]
 
     let currentSlideIdx = 0;
 
     function showCurrentSlide() {
-        const slideContainer = document.querySelector('.about-elsapo__carousel-slide-container');
+        const slideContainer = document.querySelector('.products-slides__carousel-slide-container');
         slideContainer.innerHTML = slides[currentSlideIdx];
     }
 
@@ -37,10 +49,10 @@
 
     showCurrentSlide();
 
-    document.querySelector('.about-elsapo__carousel-next')
+    document.querySelector('.products-slides__carousel-next')
          .addEventListener('click', showNextSlide);
 
-    document.querySelector('.about-elsapo__carousel-prev')
+    document.querySelector('.products-slides__carousel-prev')
          .addEventListener('click', showPrevSlide);
 
     setInterval(showNextSlide, 3000);
