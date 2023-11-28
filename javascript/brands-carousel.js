492707ebca2b7cca21e7ec6940f1ff0bb37ae6b3
Eugenia Burakova <evzickabur@gmail.com>
2023-11-29 00:33:35 +0200

    contacts block started

--- javascript/brands-carousel.js
+++ javascript/brands-carousel.js
@@ -1,16 +1,16 @@
-// slidr.create('slidr-id', {
-//     after: function(e) {console.log('in: ' + e.in.slidr);},
-//     before: function(e) {console.log('out: ' + e.out.slidr);},
-//     bredcrumbs: false,
-//     controls: 'border',
-//     direction: 'horizontal',
-//     fade: false,
-//     keyboard: true,
-//     overflow: true,
-//     theme: rgba(0, 0, 0, 0.96),
-//     timing: {'cube': '0,5s ease-in'},
-//     touch: true,
-//     transition: 'cube'
-// }).start();
+slidr.create('slidr-id', {
+    after: function(e) {console.log('in: ' + e.in.slidr);},
+    before: function(e) {console.log('out: ' + e.out.slidr);},
+    bredcrumbs: false,
+    controls: 'border',
+    direction: 'horizontal',
+    fade: false,
+    keyboard: true,
+    overflow: true,
+    theme: rgba(0, 0, 0, 0.96),
+    timing: {'cube': '0,5s ease-in'},
+    touch: true,
+    transition: 'cube'
+}).start();
 
   
