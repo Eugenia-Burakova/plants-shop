5b7cee1e7cc483ca3dc511db14ec51f7f2e252a0
Eugenia Burakova <evzickabur@gmail.com>
2023-12-06 21:23:45 +0200

    navigation burger fixed

--- javascript/brands-carousel.js
+++ javascript/brands-carousel.js
@@ -14,12 +14,12 @@ function currentSlide(n) {
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("brands-carousel__img-box");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  
-  slides[slideIndex-1].style.display = "block";
+
+  slides[slideIndex - 1].style.display = "block";
 }
   
