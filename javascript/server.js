41959094f541c9a582767cc63353f457dacc8dce
Eugenia Burakova <evzickabur@gmail.com>
2023-12-19 19:12:19 +0200

    server for contacts form  adding

---
+++ javascript/server.js
@@ -0,0 +1,37 @@
+// Usage:
+// 0. Initialize package.json
+//    npm init -y
+// 1. Install dependencies: 
+//    npm i express body-parser
+// 2. Run server:
+//    node server.js
+
+const express = require('express'); //підключаю express.js
+const bodyParser = require('body-parser'); //підключаю body-parser
+const path = require('path'); //підключаю вбудований модуль path
+const app = express(); //ініціалізуємо express
+
+app.use(bodyParser.json()); //задає використання body-parser
+app.use(express.static(path.resolve(__dirname, './'))); //альтернатива девсерверу (починає обслуговувати файли)
+
+app.post('/contact', (req, res) => { //app.post - endpoint (api на бекенді, яка буде приймати запит на url /contact)
+                                     //файлу contact у нас нема в проекті, 
+                                     //але бекенд буде чекати запит методом post на адресу ./contacts.txt
+  const fs = require('fs');
+  fs.appendFile('./contacts.txt', JSON.stringify(req.body) + '\n', function(err) { //і буде додавати інформацію, яку відправимо на адресу у файл ./contacts.txt
+    if (err) {
+      res.status(500).send('Server error');
+      return console.log(err);
+    }
+    console.log('Data saved: ' + JSON.stringify(req.body));
+    res.send('Data saved');
+  });
+});
+
+console.log(
+    'Server is running on',
+    process.env.PORT || 3000,
+    process.env.IP || '0.0.0.0'
+  );
+  
+app.listen(process.env.PORT || 3000, process.env.IP || '0.0.0.0');
